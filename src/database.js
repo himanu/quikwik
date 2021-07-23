bac91c34193a152fac0ecf53c86712a98b2172be
himanu <hy287719@gmail.com>
2021-07-23 16:49:30 +0530

    COmmented firebase emulator in database.js

--- src/database.js
+++ src/database.js
@@ -13,36 +13,36 @@ const firebaseConfig = {
     appId: "1:502922511832:web:d23f92c33f6aa2620d5ead"
 };
 firebase.initializeApp(firebaseConfig);
-var firebaseEmulators = {
-    "database": {
-      "host": "localhost",
-      "port": 9000
-    },
-    "functions": {
-      "host": "localhost",
-      "port": 5001
-    }
-  };
-if (firebaseEmulators) {
-    console.log("Automatically connecting Firebase SDKs to running emulators:");
-    Object.keys(firebaseEmulators).forEach(function(key) {
-    console.log('\t' + key + ': http://' +  firebaseEmulators[key].host + ':' + firebaseEmulators[key].port );
-    });
-    if (firebaseEmulators.database && typeof firebase.database === 'function') {
-    firebase.database().useEmulator(firebaseEmulators.database.host, firebaseEmulators.database.port);
-    }
-    if (firebaseEmulators.firestore && typeof firebase.firestore === 'function') {
-    firebase.firestore().useEmulator(firebaseEmulators.firestore.host, firebaseEmulators.firestore.port);
-    }
-    if (firebaseEmulators.functions && typeof firebase.functions === 'function') {
-    firebase.functions().useEmulator(firebaseEmulators.functions.host, firebaseEmulators.functions.port);
-    }
-    if (firebaseEmulators.auth && typeof firebase.auth === 'function') {
-    firebase.auth().useEmulator('http://' + firebaseEmulators.auth.host + ':' + firebaseEmulators.auth.port);
-    }
-} else {
-    console.log("To automatically connect the Firebase SDKs to running emulators, replace '/__/firebase/init.js' with '/__/firebase/init.js?useEmulator=true' in your index.html");
-}
+// var firebaseEmulators = {
+//     "database": {
+//       "host": "localhost",
+//       "port": 9000
+//     },
+//     "functions": {
+//       "host": "localhost",
+//       "port": 5001
+//     }
+//   };
+// if (firebaseEmulators) {
+//     console.log("Automatically connecting Firebase SDKs to running emulators:");
+//     Object.keys(firebaseEmulators).forEach(function(key) {
+//     console.log('\t' + key + ': http://' +  firebaseEmulators[key].host + ':' + firebaseEmulators[key].port );
+//     });
+//     if (firebaseEmulators.database && typeof firebase.database === 'function') {
+//     firebase.database().useEmulator(firebaseEmulators.database.host, firebaseEmulators.database.port);
+//     }
+//     if (firebaseEmulators.firestore && typeof firebase.firestore === 'function') {
+//     firebase.firestore().useEmulator(firebaseEmulators.firestore.host, firebaseEmulators.firestore.port);
+//     }
+//     if (firebaseEmulators.functions && typeof firebase.functions === 'function') {
+//     firebase.functions().useEmulator(firebaseEmulators.functions.host, firebaseEmulators.functions.port);
+//     }
+//     if (firebaseEmulators.auth && typeof firebase.auth === 'function') {
+//     firebase.auth().useEmulator('http://' + firebaseEmulators.auth.host + ':' + firebaseEmulators.auth.port);
+//     }
+// } else {
+//     console.log("To automatically connect the Firebase SDKs to running emulators, replace '/__/firebase/init.js' with '/__/firebase/init.js?useEmulator=true' in your index.html");
+// }
   
 
 let roundValue = 1;
