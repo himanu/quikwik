16d15830ee985ab06a5e0dd1ed2ad4491bbfe30c
Asim Aashish <asimaashish1234@gmail.com>
2021-08-03 15:17:45 +0530

    add quikwik to prod

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
-import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import css from 'rollup-plugin-css-only';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import css from "rollup-plugin-css-only";
+import injectProcessEnv from "rollup-plugin-inject-process-env";
+import livereload from "rollup-plugin-livereload";
+import svelte from "rollup-plugin-svelte";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
-
+const EMULATE = !!process.env.EMULATE;
 function serve() {
 	let server;
 
@@ -17,35 +19,36 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
+			server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+				stdio: ["ignore", "inherit", "inherit"],
+				shell: true,
 			});
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
+			process.on("SIGTERM", toExit);
+			process.on("exit", toExit);
+		},
 	};
 }
 
 export default {
-	input: 'src/main.js',
+	input: "src/main.js",
 	output: {
 		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
+		format: "iife",
+		name: "app",
+		file: "public/build/bundle.js",
 	},
 	plugins: [
 		svelte({
+			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
 				// enable run-time checks when not in production
-				dev: !production
-			}
+				dev: !production,
+			},
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: "bundle.css" }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -54,23 +57,32 @@ export default {
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte']
+			dedupe: ["svelte"],
 		}),
 		commonjs(),
+		injectProcessEnv({
+			API_KEY: process.env.API_KEY,
+			AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+			DATABASE_URL: process.env.DATABASE_URL,
+			PROJECT_ID: process.env.PROJECT_ID,
+			STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+			MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+			APP_ID: process.env.APP_ID,
+		}),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && serve(),
+		!production && !EMULATE && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload("public"),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
 	],
 	watch: {
-		clearScreen: false
-	}
+		clearScreen: false,
+	},
 };
---
+++ scripts/deploy_dev.sh
@@ -0,0 +1,6 @@
+firebase use dev
+export $(xargs < .env.development)
+firebase functions:config:set app.id=$DAPP_ID
+firebase functions:config:set app.secret=$DAPP_SECRET
+firebase functions:config:set app.url=$DAPP_URL
+firebase deploy
